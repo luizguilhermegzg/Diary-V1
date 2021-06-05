63f7e2ed2c7c31b04f838da9e6fdc39b8d0734ee
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-05 20:49:50 -0300

    Diary V1: error when close the open window to solve

---
+++ main.py
@@ -0,0 +1,77 @@
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import InputText, WIN_CLOSED, Window
+sg.theme('Reddit')
+
+
+def diary_window():
+    layout= [
+        [sg.Button('NEW', size=(7,1), font='book-antiqua 14', button_color='#658EA9'), sg.Button('SAVE', size=(7,1), font='book-antiqua 14', button_color='#88B2CC'), sg.Button('OPEN', size=(7,1), font='book-antiqua 14', button_color='#E7D4C0'), sg.Button('DELETE', size=(7,1), font='book-antiqua 14', button_color='#E98973'), sg.Text('This software was created by Cricket Softwares and Services', background_color='aquamarine')],
+        [sg.Multiline(size=(120, 30), key='textbox', font='calibri 12', background_color='#E7F2F8')]
+    ]
+    screen = sg.Window('Diary V1', layout, background_color='aquamarine').finalize()
+    interations(screen)
+
+
+def open_new_archive(text):
+    layout= [
+        [sg.Button('NEW', size=(7,1), font='book-antiqua 14', button_color='#658EA9'), sg.Button('SAVE', size=(7,1), font='book-antiqua 14', button_color='#88B2CC'), sg.Button('OPEN', size=(7,1), font='book-antiqua 14', button_color='#E7D4C0'), sg.Button('DELETE', size=(7,1), font='book-antiqua 14', button_color='#E98973'), sg.Text('This software was created by Cricket Softwares and Services', background_color='aquamarine')],
+        [sg.Multiline(text ,size=(120, 30), key='textbox', font='calibri 12', background_color='#E7F2F8')]
+    ]
+    screen = sg.Window("Diary V",layout, background_color='aquamarine').finalize()
+    interations(screen)
+
+
+
+def interations(window):
+    while True:
+        events, values = window.read()
+        if events == sg.WIN_CLOSED:
+            break
+        if events == 'NEW':
+            diary_window()
+        if events == 'SAVE':
+                print(values['textbox'])
+                path = sg.PopupGetFolder('Choose a folder to save:')
+                if path == "" or path == None:
+                    print('nada')
+                else: 
+                    file_name_layout = [[sg.Text('Say the name of your file:'), sg.InputText(key='file_name'), sg.Button('Save')]]
+                    file_name_window = sg.Window('File name', file_name_layout)
+                    while True:
+                        file_events, file_values = file_name_window.read()
+                        if file_events == sg.WIN_CLOSED:
+                            break
+                        if file_events == 'Save' and file_values['file_name'] != "":
+                            my_file = open(path +'/'+ file_values['file_name']+ '.txt','w')
+                            my_file.write(values['textbox'])
+                            sg.popup_notify("Arquivo salvo com sucesso!!!")
+                            my_file.close() 
+                            file_name_window.close()
+                        else:
+                            sg.popup_error("You need to put a name!!!")
+        if events == 'OPEN':                              ## Problem to solve here!!!
+            filename= open(sg.popup_get_file('Enter the file you wish to process'))
+            if filename == "" or filename == None:
+                print('nada')
+            
+            else:
+                extracted_text = filename.read()
+                window.close()
+                open_new_archive(extracted_text)
+        if events == 'DELETE':
+            window.close()
+            open_new_archive('')
+
+
+
+diary_window()
+
+
+
+
+
+
+
+
+
+
