493e5bb28bc488865477986cdb1d3bd6edbd374e
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-06 13:00:16 -0300

    somes comments addictions to the project

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+########################################################              IMPORTANT DISCLAIMER!!!!!!!!!!!!!!!!!!!                  ###################################################################
+# The program have a bug unsolved in the "OPEN" interation function. The bug is:  When you click in any button of the window without selecting a path the program close automaticaly.
+# If you know and wanna solve this problem, please make a pull request, i will verify and push to the main project if the problem be solved. Thank you all!
+########################################################              IMPORTS                  ###################################################################
+
 import PySimpleGUI as sg
 from PySimpleGUI.PySimpleGUI import InputText, WIN_CLOSED, Window
 sg.theme('Reddit')
 
-
+########################################################              WINDOWS                  ###################################################################
 def diary_window():
     layout= [
-        [sg.Button('NEW', size=(7,1), font='book-antiqua 14', button_color='#658EA9'), sg.Button('SAVE', size=(7,1), font='book-antiqua 14', button_color='#88B2CC'), sg.Button('OPEN', size=(7,1), font='book-antiqua 14', button_color='#E7D4C0'), sg.Button('DELETE', size=(7,1), font='book-antiqua 14', button_color='#E98973'), sg.Text('This software was created by Cricket Softwares and Services', background_color='aquamarine')],
-        [sg.Multiline(size=(120, 30), key='textbox', font='calibri 12', background_color='#E7F2F8')]
+         [sg.Button('NEW', size=(7,1), font='book-antiqua 14', button_color='#658EA9'), sg.Button('SAVE', size=(7,1), font='book-antiqua 14', button_color='#88B2CC'), sg.Button('OPEN', size=(7,1), font='book-antiqua 14', button_color='#E7D4C0'), sg.Button('DELETE', size=(7,1), font='book-antiqua 14', button_color='#E98973'), sg.Text('This software was created by Cricket Softwares and Services', background_color='aquamarine')],
+         [sg.Multiline(size=(120, 30), key='textbox', font='calibri 12', background_color='#E7F2F8')]
     ]
     screen = sg.Window('Diary V1', layout, background_color='aquamarine').finalize()
     interations(screen)
@@ -21,7 +26,7 @@ def open_new_archive(text):
     interations(screen)
 
 
-
+########################################################              CATCHING EVENTS, VALUES AND ETC                  ###################################################################
 def interations(window):
     while True:
         events, values = window.read()
@@ -30,9 +35,8 @@ def interations(window):
         if events == 'NEW':
             diary_window()
         if events == 'SAVE':
-                print(values['textbox'])
                 path = sg.PopupGetFolder('Choose a folder to save:')
-                if path == "" or path == None:
+                if path == "":
                     print('nada')
                 else: 
                     file_name_layout = [[sg.Text('Say the name of your file:'), sg.InputText(key='file_name'), sg.Button('Save')]]
@@ -53,7 +57,6 @@ def interations(window):
             filename= open(sg.popup_get_file('Enter the file you wish to process'))
             if filename == "" or filename == None:
                 print('nada')
-            
             else:
                 extracted_text = filename.read()
                 window.close()
@@ -61,11 +64,8 @@ def interations(window):
         if events == 'DELETE':
             window.close()
             open_new_archive('')
-
-
-
+########################################################              CALL MAIN WINDOW                  ###################################################################
 diary_window()
-
 
 
 
